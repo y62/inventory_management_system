ecd11b2a718ecb50dd045955d927682d2cdd8d61
mariamawuena <mari543k@stud.kea.dk>
2020-06-29 22:11:54 +0200

    New commit

--- src/main/java/ims/app/controllers/HomeController.java
+++ src/main/java/ims/app/controllers/HomeController.java
@@ -1,14 +1,25 @@
 package ims.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private String signin = "home/login";
     private String home = "home/home";
     private String comingSoon = "home/coming-soon";
     private String settings = "settings/settings-page";
+
+
+    @GetMapping("/home/login")
+    public String signin(String error, Model model) {
+        if (error != null) {
+            model.addAttribute("ErrorParam", true);
+        }
+        return signin;
+    }
 
     @GetMapping("/")
     public String getHome() {
