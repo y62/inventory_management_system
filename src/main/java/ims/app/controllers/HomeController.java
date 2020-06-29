d08b5c96db20c89c247b57c94fd0d29667926401
mariamawuena <mari543k@stud.kea.dk>
2020-06-29 20:24:04 +0200

    New edits

--- src/main/java/ims/app/controllers/HomeController.java
+++ src/main/java/ims/app/controllers/HomeController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private String home = "home/home";
+    private String comingSoon = "home/coming-soon";
     private String settings = "settings/settings-page";
 
     @GetMapping("/")
     public String getHome() {
         return home;
+    }
+
+    @GetMapping("/home/coming-soon")
+    public String getComingSoon() {
+        return comingSoon;
     }
 
     @GetMapping("/settings/settings-page")
